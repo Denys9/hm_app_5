5cee8d8ab0bb80d771131a0ff0cb9ab79001c9fc
lozin <lozindenis6@gmail.com>
2022-10-15 12:14:57 +0300

    Create work "№2"

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import math
 
 try:
     for i in range(1,10):
         for j in range(1,10):
             print(f'{i}*{j}=',i*j,end="\t")
         print('\n')
-
-
-
-
-
-
-
 except Exception as ex:
     print(f'Eror information: {ex}')
